041dd29fe58ef990f8786ecbd0d6af8a8e929e8d
Md Rashed Hossain <wprashed@icloud.com>
2026-04-01 12:31:37 +0600

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -12,21 +12,21 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 global $wpdb;
 
 $option_names = array(
-	'crt_duration',
-	'crt_show_on',
-	'crt_position',
-	'crt_color_scheme',
-	'crt_min_cart',
-	'crt_show_progress',
-	'crt_dismissable',
-	'crt_discount_type',
-	'crt_discount_amount',
-	'crt_message_user',
-	'crt_message_guest',
-	'crt_message_user_b',
-	'crt_message_guest_b',
-	'crt_enable_sound',
-	'crt_enable_email',
+	'dealcare_crt_duration',
+	'dealcare_crt_show_on',
+	'dealcare_crt_position',
+	'dealcare_crt_color_scheme',
+	'dealcare_crt_min_cart',
+	'dealcare_crt_show_progress',
+	'dealcare_crt_dismissable',
+	'dealcare_crt_discount_type',
+	'dealcare_crt_discount_amount',
+	'dealcare_crt_message_user',
+	'dealcare_crt_message_guest',
+	'dealcare_crt_message_user_b',
+	'dealcare_crt_message_guest_b',
+	'dealcare_crt_enable_sound',
+	'dealcare_crt_enable_email',
 );
 
 foreach ( $option_names as $option_name ) {
@@ -34,8 +34,8 @@ foreach ( $option_names as $option_name ) {
 	delete_site_option( $option_name );
 }
 
-wp_clear_scheduled_hook( 'crt_send_email_reminders' );
+wp_clear_scheduled_hook( 'dealcare_crt_send_email_reminders' );
 
-$table_name = $wpdb->prefix . 'crt_abandoned_carts';
+$table_name = $wpdb->prefix . 'dealcare_crt_abandoned_carts';
 $table_name_sql = esc_sql( $table_name );
 $wpdb->query( "DROP TABLE IF EXISTS `{$table_name_sql}`" ); // phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery,WordPress.DB.DirectDatabaseQuery.NoCaching,WordPress.DB.DirectDatabaseQuery.SchemaChange,WordPress.DB.PreparedSQL.NotPrepared
